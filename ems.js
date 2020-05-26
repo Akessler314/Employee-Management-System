79539c0851ecf0f8d30d9445f59c4fafac5e3542
Andrew Kessler <andrew.kessler95@gmail.com>
2020-05-25 21:03:22 -0600

    took the stand alone functions and added them directly to switch statements to avoid conflicts with connection.querry not being a function and issues with .then due to that

--- ems.js
+++ ems.js
@@ -1,52 +1,225 @@
-//DEPENDENCIES 
-const inquirer = require('inquirer');
 const mysql = require("mysql");
-const consoleTable = require('console.table');
+const cTable = require("console.table");
+const inquirer = require("inquirer");
 
-//DATABASE CONNECTION INFORMATION 
-const connection = mysql.createConnection({
-  host: "localhost",
-
-  port: 3306,
-
-  user: "root",
-
-  password: "",
-  database: "employee_management_systemDB"
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_management_systemDB"
 });
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  //call first function here -- remeber on the last function called to have "connection.end();"
-});
-
-
-
-
-
-//----------------------------------------------------------------------------
-// Functions 
-
-// function to add a department 
-function newDepartment(){
-inquirer.prompt(
-//inquired prompt to get the name of the department being added 
-{
-type: 'input',
-name: 'newDepartment',
-message: 'Enter the name of the new department'
+//Valadite to make sure the expected string response is not a number 
+function validateString(answer) {
+    if (answer != "" && isNaN(parseInt(answer))) {
+        return true;
+    }
+    return false;
 }
-)
-.then((response) => {
-connection.query(
-  'INSERT INTO department SET ?', 
-  {name : response.newDepartment}, 
-  function(err){
-    if (err) throw err
-      console.log('New department added to table');
-      //go back to init function to promt user what they would liek to do next 
-      //DNE yet 
-  });
-});
-};
+//Validation to make sure an expected number result is not a string 
+function validateNumber(answer) {
+    if (answer != "" && !isNaN(parseInt(answer))) {
+        return true;
+    }
+    return false;
+}
+//function ran at the end of each switch case to let user exit the application OR continue 
+function continuePrompt() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "continue",
+            message: "Would you like to coninue?"
+        }
+    ]).then(function (data) {
+        if (data.continue) {
+            main();
+        }
+        else {
+            return;
+        }
+    });
+}
+// Main propt to find out what the user would like to do, uses switch case after to continue. 
+function main() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "mainMenu",
+            message: "Select an option:",
+            choices: [
+                "View All Employees", //added 
+                "View All Roles", //added
+                "View All Departments", //added
+                "Add An Employee", // use the update one as a reference 
+                "Add A Role", //added 
+                "Add A Department", //added 
+                "Update Employee Role" //added 
+            ]
+        }
+    ]).then(function (answer) {
+        switch (answer.mainMenu) {
+            //Shows all Employess 
+            case "View All Employees":
+                var query = connection.query("SELECT * FROM employee", function (err, data) {
+                    if (err) throw err;
+                    console.table(data);
+                    continuePrompt();
+                });
+                break;
+            //Shows all departments 
+            case "View All Departments":
+                var query = connection.query("SELECT * FROM department", function (err, data) {
+                    if (err) throw err;
+                    console.table(data);
+                    continuePrompt();
+                });
+                break;
+                //Shows all roles 
+            case "View All Roles":
+                var query = connection.query("SELECT * FROM role", function (err, data) {
+                    if (err) throw err;
+                    console.table(data);
+                    continuePrompt();
+                });
+                break;
+                //Adds a new role to the the role DB 
+            case "Add A Role":
+                var query = connection.query("SELECT id, department FROM department", function (err, data) {
+                    if (err) throw err;
+                    let choices = data.map(x => `${x.id} - ${x.department}`);
+                    inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "title",
+                            message: "Enter the role name:",
+                            validate: validateString
+                        },
+                        {
+                            type: "input",
+                            name: "salary",
+                            message: "Enter the salary:",
+                            validate: validateNumber
+                        },
+                        {
+                            type: "list",
+                            name: "department",
+                            message: "Select the department:",
+                            choices: [...choices]
+                        }
+                    ]).then(function (data) {
+                        var arr = data.department.split(" ");
+                        var deptID = parseInt(arr[0]);
+                        var query = connection.query(`INSERT INTO role (title, salary, department_id) VALUES ('${data.title}', ${data.salary}, ${deptID})`, function (err, data) {
+                            if (err) throw err;
+                            continuePrompt();
+                        });
+                    });
+                });
+                break;
+
+
+            case "Add A Department":
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "department",
+                        message: "Enter the department's name:",
+                        validate: validateString
+                    }
+                ]).then(function (data) {
+                    var query = connection.query(`INSERT INTO department (department) VALUES ('${data.department}');`, function (err, data) {
+                        if (err) throw err;
+                        return data;
+                    });
+                    continuePrompt();
+                });
+                break;
+
+
+            case "Update Employee Role":
+                const emp = {
+                    first_name: "",
+                    last_name: "",
+                    role_id: 0,
+                    manager_id: 0,
+                    empID: 0
+                };
+                var query = connection.query("SELECT id, first_name, last_name FROM employee", function (err, data) {
+                    if (err) throw err;
+                    let choices = data.map(x => `${x.id} - ${x.first_name} ${x.last_name}`);
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            name: "employee",
+                            message: "Select an employee:",
+                            choices: [...choices]
+                        }
+                    ]).then(function (data) {
+                        var arr = data.employee.split(" ");
+                        emp.empID = parseInt(arr[0]);
+                        inquirer.prompt([
+                            {
+                                type: "input",
+                                name: "firstName",
+                                message: "Enter the employee's first name:",
+                                validate: validateString
+                            },
+                            {
+                                type: "input",
+                                name: "lastName",
+                                message: "Enter the employee's last name:",
+                                validate: validateString
+                            }
+                        ]).then(function (data) {
+                            emp.first_name = data.firstName;
+                            emp.last_name = data.lastName;
+                            var query = connection.query("SELECT id, title FROM role", function (err, data) {
+                                if (err) throw err;
+                                let choices = data.map(x => `${x.id} - ${x.title}`);
+                                inquirer.prompt([
+                                    {
+                                        type: "list",
+                                        name: "title",
+                                        message: "Select a title:",
+                                        choices: [...choices]
+                                    }
+                                ]).then(function (data) {
+                                    var arr = data.title.split(" ");
+                                    emp.role_id = parseInt(arr[0]);
+                                    var query = connection.query("SELECT id, first_name, last_name FROM employee", function (err, data) {
+                                        if (err) throw err;
+                                        let choices = data.map(x => `${x.id} - ${x.first_name} ${x.last_name}`);
+                                        choices.push("This employee does not have a manager");
+                                        inquirer.prompt([
+                                            {
+                                                type: "list",
+                                                name: "manager",
+                                                message: "Select this employee's manager:",
+                                                choices: [...choices]
+                                            }
+                                        ]).then(function (data) {
+                                            if (data.manager === "This employee does not have a manager") {
+                                                emp.manager_id = null;
+                                            }
+                                            else {
+                                                var arr = data.manager.split(" ");
+                                                emp.manager_id = parseInt(arr[0]);
+                                            }
+                                            var query = connection.query(`UPDATE employee SET first_name = '${emp.first_name}', last_name = '${emp.last_name}', role_id = ${emp.role_id}, manager_id = ${emp.manager_id} WHERE id = ${emp.empID}`, function (err, data) {
+                                                if (err) throw err;
+                                                continuePrompt();
+                                                return data;
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+                break;
+        }
+    });
+}
+main();
--- schema.sql
+++ schema.sql
@@ -5,13 +5,13 @@ CREATE database employee_management_systemDB;
 USE employee_management_systemDB;
 
 CREATE TABLE department (
-  id INT NOT NULL,
-  name VARCHAR(30) NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  department VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,4) NOT NULL, 
   department_id INT NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
- id INT NOT NULL,
+ id INT NOT NULL AUTO_INCREMENT,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL, 
